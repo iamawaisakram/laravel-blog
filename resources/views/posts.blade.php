ab9147b1f0f44130a556990581a3dd2efa6e7089
Awais Mughal <awais.akram@empglabs.com>
2021-06-17 11:41:53 +0500

    Blog Home Page Routing with Multiple Blogs Routing

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<title>My Blog</title>
+
+<link rel="stylesheet" href="/app.css" >
+
+<body>
+    <article>
+        <h1>
+            <a href="/posts/my-first-post">My First Post</a>
+
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quos quis quo ea voluptas fugiat exercitationem asperiores enim,
+            commodi porro quam officia eius laboriosam. Odit recusandae
+            facere esse tenetur, voluptatibus ducimus.
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/posts/my-second-post">My Second Post</a>
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quos quis quo ea voluptas fugiat exercitationem asperiores enim,
+            commodi porro quam officia eius laboriosam. Odit recusandae
+            facere esse tenetur, voluptatibus ducimus.
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/posts/my-third-post">My Third Post</a>
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quos quis quo ea voluptas fugiat exercitationem asperiores enim,
+            commodi porro quam officia eius laboriosam. Odit recusandae
+            facere esse tenetur, voluptatibus ducimus.
+        </p>
+    </article>
+</body>
