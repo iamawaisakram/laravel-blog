bea90b241c730ac632764cb185c1764656bc2d97
Awais Mughal <awais.akram@empglabs.com>
2021-07-27 15:33:18 +0500

    DB seeding and User relation with Posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $hobbies = Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies'
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My Family Post',
+            'excerpt' => 'Excerpt for my First post',
+            'slug' => "my-family-post",
+            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'excerpt' => 'Excerpt for my Second post',
+            'slug' => "my-work-post",
+            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hobbies->id,
+            'title' => 'My Hobby Post',
+            'excerpt' => 'Excerpt for my Third post',
+            'slug' => "my-hobby-post",
+            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
+        ]);
     }
 }
