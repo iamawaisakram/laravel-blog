0999ac477ca1e02faf3a9d497f4b35ba12673645
Awais Mughal <awais.akram@empglabs.com>
2021-07-27 12:34:38 +0500

    Route using slug

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 });
 
 
-Route::get('posts/{post}', function ($id) {
-    return view('post', ['post' => Post::findOrFail($id)]);
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
 });
