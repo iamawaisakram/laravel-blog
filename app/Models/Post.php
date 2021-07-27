03b81ca8a559ec0dbc9791e3dedeacca5e252b57
Awais Mughal <awais.akram@empglabs.com>
2021-07-27 11:39:06 +0500

    Make mass assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // * Don't guard anything and disallow any mass assignment using array
+    // * Unless you're in complete control of it.
+    protected $guarded = [];
+
+    // * Guard certain properties while allowing mass assignment
+    // ! protected $guarded = ['id'];
+
+    // * Specify properties that you want to mass assign
+    // ! protected $fillable = ['title', 'excerpt', 'body'];
 }
