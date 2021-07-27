b9bcf8837c92fc6c9b39f2f79d7ab837cae6e9fd
Awais Mughal <awais.akram@empglabs.com>
2021-07-27 14:55:35 +0500

    Post category Association

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +16,20 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/', function () {
-    return view('posts', ['posts' => Post::all()]);
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
