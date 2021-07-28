32acc810647404707f94f8bb26cb809dfc213896
Awais Mughal <awais.akram@empglabs.com>
2021-07-28 13:14:47 +0500

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,53 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Family Post',
-            'excerpt' => 'Excerpt for my First post',
-            'slug' => "my-family-post",
-            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'excerpt' => 'Excerpt for my Second post',
-            'slug' => "my-work-post",
-            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My Hobby Post',
-            'excerpt' => 'Excerpt for my Third post',
-            'slug' => "my-hobby-post",
-            'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
